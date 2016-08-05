9c5c1d58aa6be65f541992163eee3153745907fb
yagiri000 <yohakuari@gmail.com>
2016-08-05 18:22:09 +0900

    TriMesh完成、関連クラスプロジェクト内に作成

---
+++ N2_3DGameLibrary/Renderer.cpp
@@ -0,0 +1,246 @@
+#include <assert.h>
+
+#include "CommonStuff.h"
+#include "Renderer.h"
+#include "MathUtil.h"
+
+Renderer::Renderer()
+{
+}
+
+int Renderer::getVideoModeCount()
+{
+	return 0;
+}
+
+const VideoMode & Renderer::getVideoMode(int index)
+{
+	// TODO: return ステートメントをここに挿入します
+}
+
+void Renderer::init(const VideoMode & mode)
+{
+}
+
+void Renderer::shutdown()
+{
+}
+
+void Renderer::flipPages()
+{
+}
+
+void Renderer::beginScene()
+{
+}
+
+void Renderer::endScene()
+{
+}
+
+void Renderer::videoSave()
+{
+}
+
+void Renderer::videoRestore()
+{
+}
+
+void Renderer::setCamera(const Vector3 & pos, const EulerAngles & orient)
+{
+}
+
+void Renderer::setZoom(float xZoom, float yZoom)
+{
+}
+
+void Renderer::setNearFarClippingPlanes(float n, float f)
+{
+}
+
+void Renderer::setWindow(int x1, int y1, int xSize, int ySize)
+{
+}
+
+void Renderer::setFullScreenWindow()
+{
+}
+
+void Renderer::getWindow(int * x1, int * y1, int * xSize, int * ySize)
+{
+}
+
+void Renderer::instance(const Vector3 & pos, const EulerAngles & orient)
+{
+}
+
+void Renderer::instancePop()
+{
+}
+
+void Renderer::resetTextureCache()
+{
+}
+
+int Renderer::findTexture(const char * name)
+{
+	return 0;
+}
+
+int Renderer::allocTexture(const char * name, int xSize, int ySize)
+{
+	return 0;
+}
+
+void Renderer::freeTexture(int handle)
+{
+}
+
+void Renderer::setTextureImage(int handle, const unsigned * image)
+{
+}
+
+int Renderer::cacheTexture(const char * filename)
+{
+	return 0;
+}
+
+void Renderer::cacheTexture(TextureReference & texture)
+{
+}
+
+void Renderer::setDepthBufferMode(bool readEnabled, bool writeEnabled)
+{
+}
+
+void Renderer::setBlendEnable(bool blend)
+{
+}
+
+void Renderer::setSourceBlendMode(ESourceBlendMode mode)
+{
+}
+
+void Renderer::setDestBlendMode(EDestBlendMode mode)
+{
+}
+
+void Renderer::setRGB(unsigned int rgb)
+{
+}
+
+void Renderer::setARGB(unsigned argb)
+{
+}
+
+void Renderer::setOpacity(float a)
+{
+}
+
+void Renderer::setFogEnable(bool flag)
+{
+}
+
+void Renderer::setFogColor(unsigned rgb)
+{
+}
+
+void Renderer::setFogDistance(float nearFog, float farFog)
+{
+}
+
+void Renderer::setAmbientLightColor(unsigned rgb)
+{
+}
+
+void Renderer::setDirectionalLightVector(const Vector3 & v)
+{
+}
+
+void Renderer::setDirectionalLightColor(unsigned rgb)
+{
+}
+
+void Renderer::setLightEnable(bool flag)
+{
+}
+
+void Renderer::setBackfaceMode(EBackfaceMode mode)
+{
+}
+
+void Renderer::selectTexture(int handle)
+{
+}
+
+void Renderer::selectTexture(TextureReference & texture)
+{
+}
+
+void Renderer::setTextureClamp(bool flag)
+{
+}
+
+void Renderer::clear(int options)
+{
+}
+
+void Renderer::renderTriMesh(const RenderVertex * vertexList, int vertexCount, const RenderTri * triList, int triCount)
+{
+}
+
+void Renderer::renderTriMesh(const RenderVertexL * vertexList, int vertexCount, const RenderTri * triList, int triCount)
+{
+}
+
+void Renderer::renderTriMesh(const RenderVertexTL * vertexList, int vertexCount, const RenderTri * triList, int triCount)
+{
+}
+
+void Renderer::dot(int x, int y)
+{
+}
+
+void Renderer::line(int x1, int y1, int x2, int y2)
+{
+}
+
+void Renderer::boxFill(int x1, int y1, int x2, int y2)
+{
+}
+
+const Matrix4x3 & Renderer::getModelToCameraMatrix()
+{
+	// TODO: return ステートメントをここに挿入します
+}
+
+const Matrix4x3 & Renderer::getModelToWorldMatrix()
+{
+	// TODO: return ステートメントをここに挿入します
+}
+
+int Renderer::computeOutCode(const Vector3 & p)
+{
+	return 0;
+}
+
+int Renderer::projectPoint(const Vector3 & p, Vector3 * result)
+{
+	return 0;
+}
+
+void Renderer::updateModelToWorldMatrix()
+{
+}
+
+void Renderer::computeClipMatrix()
+{
+}
+
+void Renderer::getModelToClipMatrix()
+{
+}
+
+void Renderer::freeAllTextures()
+{
+}
--- N2_3DGameLibrary/TriMesh.cpp
+++ N2_3DGameLibrary/TriMesh.cpp
@@ -1,12 +1,108 @@
 #include "TriMesh.h"
 
+#include <assert.h>
+#include <stdlib.h>
+
+#include "CommonStuff.h"
+#include "TriMesh.h"
+#include "Renderer.h"
+#include "EditTriMesh.h"
+
 
 
 TriMesh::TriMesh()
 {
+	vertexCount = 0;
+	vertexList = NULL;
+	triCount = 0;
+	triList = NULL;
+	boundingBox.empty();
 }
 
 
 TriMesh::~TriMesh()
 {
+	freeMemory();
+}
+
+void TriMesh::allocateMemory(int nVertexCount, int nTriCount)
+{
+	freeMemory();
+
+	if (nVertexCount > 65536) {
+		ABORT("allocate muri");
+	}
+	
+	vertexCount = nVertexCount;
+	vertexList = new RenderVertex[vertexCount];
+
+	triCount = nTriCount;
+	triList = new RenderTri[triCount];
+}
+
+
+void TriMesh::freeMemory()
+{
+	delete[] vertexList;
+	delete[] triList;
+
+	vertexList = NULL;
+	vertexCount = 0;
+	triList = NULL;
+	triCount = 0;
+}
+
+void TriMesh::render() const
+{
+	gRenderer.renderTriMesh(vertexList, vertexCount, triList, triCount);
+}
+
+void TriMesh::computeBoundingBox()
+{
+	boundingBox.empty();
+
+	for (int i = 0; i < vertexCount; i++) {
+		boundingBox.add(vertexList[i].p);
+	}
+}
+
+void TriMesh::fromEditMesh(const EditTriMesh & mesh)
+{
+	int i;
+
+	EditTriMesh tempMesh(mesh);
+
+	tempMesh.copyUvsIntoVerticles();
+
+	tempMesh.optimizeVertexOrder(true);
+
+	allocateMemory(tempMesh.vertexcount(), tempMesh.triCount());
+
+	if (triCount < 1) {
+		return;
+	}
+
+	for (i = 0; i < vertexCount; i++) {
+		const EditTriMesh::Vertex *s = &tempMesh.vertex(i);
+		RenderVertex *d = &vertexList[i];
+		d->p = s->p;
+		d->n = s->normal;
+		d->u = s->u;
+		d->v = s->v;
+	}
+
+	for (i = 0; i < triCount; i++) {
+		const EditTriMesh::Tri *s = &tempMesh.tri(i);
+		RenderTri *d = &triList[i];
+		d->index[0] = s->v[0].index;
+		d->index[1] = s->v[1].index;
+		d->index[2] = s->v[2].index;
+	}
+
+	computeBoundingBox();
+}
+
+void TriMesh::toEditMesh(EditTriMesh & mesh) const
+{
+	assert(false);
 }
